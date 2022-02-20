93ad5da7f3630b26b33ed08fd10efb547e00686d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-20 10:13:34 -0300

    Removendo duplicação

--- avancando_na_orientacao_a_objetos/modelo.py
+++ avancando_na_orientacao_a_objetos/modelo.py
@@ -1,47 +1,38 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def dar_like(self):
+        self._likes += 1
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_like(self):
-        self.__likes += 1
+        self._likes = 0
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_like(self):
-        self.__likes += 1
+        self._likes = 0
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
