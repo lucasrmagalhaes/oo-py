56fbe92caab0a8dbc58574e38d54cbf19b6d0e9e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-22 20:47:39 -0300

    Polimorfismo

--- avancando_na_orientacao_a_objetos/modelo.py
+++ avancando_na_orientacao_a_objetos/modelo.py
@@ -19,18 +19,15 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
-
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
-
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
@@ -41,3 +38,9 @@ dexter = Serie('dexter', 2008, 9)
 dexter.dar_like()
 
 print(f'{dexter.nome} - {dexter.temporadas}: {dexter.likes}')
+
+filmes_e_series = [vingadores, dexter]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
