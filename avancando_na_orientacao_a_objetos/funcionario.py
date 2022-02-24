86825e9e33c486fe8106ddee7306afd5852a1e0f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-23 22:18:28 -0300

    Resolução de métodos

--- avancando_na_orientacao_a_objetos/funcionario.py
+++ avancando_na_orientacao_a_objetos/funcionario.py
@@ -5,19 +5,19 @@ class Funcionario:
     def mostrar_tarefas(self):
         print('Fez muita coisa...')
 
-class Alura(Funcionario):
-    def mostrar_tarefas(self):
-        print('Fez muita coisa, Alurete!')
-
-    def busca_perguntas_sem_resposta(self):
-        print('Mostrando perguntas não respondidas do fórum')
-
 class Caelum(Funcionario):
     def mostrar_tarefas(self):
         print('Fez muita coisa, Caelumer')
 
     def busca_cursos_do_mes(self, mes=None):
         print(f'Mostrando cursos - {mes}' if mes else 'Mostrando cursos desse mês')
+
+class Alura(Funcionario):
+    # def mostrar_tarefas(self):
+    #     print('Fez muita coisa, Alurete!')
+
+    def busca_perguntas_sem_resposta(self):
+        print('Mostrando perguntas não respondidas do fórum')
 
 class Junior(Alura):
     pass
